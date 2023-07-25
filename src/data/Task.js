56267981340de8a386729cb58f2c32b4b5af75d4
PaolaCotrina <jpcotrina@indracompany.com>
2023-07-25 16:12:11 -0500

    firts commit

---
+++ src/data/Task.js
@@ -0,0 +1,17 @@
+export const tasks=[
+    {
+        id: 1,
+        title: 'Mi primera tarea',
+        description: 'descripcon de mi primera tarea'
+    },
+    {
+        id: 2,
+        title: 'Mi segunda tarea',
+        description: 'descripcon de mi segunda tarea'
+    },
+    {
+        id: 3,
+        title: 'Mi tercera tarea',
+        description: 'descripcon de mi tercera tarea'
+    }
+];
